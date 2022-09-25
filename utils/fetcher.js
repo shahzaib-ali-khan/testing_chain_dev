9d51dbcfc548869e7550e4cef870b3cc7c504a98
Shahzaib Khan <shahzaib.ali.k39@gmail.com>
2022-09-25 23:14:52 +0200

    Headers added to utils/fetcher

--- utils/fetcher.js
+++ utils/fetcher.js
@@ -2,6 +2,14 @@ import fetch from "isomorphic-unfetch";
 
 // eslint-disable-next-line
 export default async function Fetcher(...args) {
-  const res = await fetch(...args);
+  const res = await fetch(...args,
+    {
+      method: "GET",
+      headers: {
+        // update with your user-agent
+        "User-Agent": "*", 
+        Accept: "application/json; charset=UTF-8",
+      }
+    });
   return res.json();
 }
